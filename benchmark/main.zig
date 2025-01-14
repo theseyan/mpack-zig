0a525ffe7a81a09af5db08613cef9e8abba2c6ac
theseyan <sayanjyotidasworkmail@gmai.com>
2025-01-15 01:01:40 +0530

    add some benchmarks

--- benchmark/main.zig
+++ benchmark/main.zig
@@ -1,119 +1,242 @@
-/// TODO: Write benchmarks
-/// The current code is just me toying around.
-
 const std = @import("std");
 const mpack = @import("mpack");
-const Reader = mpack.Reader;
-const Cursor = mpack.Cursor;
-const Writer = mpack.Writer;
+const zbench = @import("zbench");
 
-// c_allocator has best performance
-const allocator = std.heap.c_allocator;
+const alloc = std.heap.c_allocator;
 
-// // Some data structures
-// const Point = struct {
-//     x: f64,
-//     y: f64,
-// };
+var readBuffer: [1024]u8 = undefined;
+var nodePool: mpack.Tree.Pool = undefined;
 
-// const Person = struct {
-//     name: []const u8,
-//     age: u32,
-//     is_active: bool,
-//     location: ?Point,
-//     tags: []const []const u8,
-//     points: []const Point
-// };
+fn prepareReadBuffer() void {
+    var writer = mpack.Writer.init(&readBuffer);
 
-// pub fn main() !void {
-//     var buffer: [1024]u8 = undefined;
-//     var writer = Writer.init(&buffer);
+    const Point = struct {
+        x: f64,
+        y: f64,
+    };
 
-//     const person = Person{
-//         .name = "John Doe",
-//         .age = 30,
-//         .is_active = true,
-//         .location = Point{ .x = 10.5, .y = 20.7 },
-//         .tags = &[_][]const u8{ "developer", "musician" },
-//         .points =  &[_]Point{
-//             Point{
-//                 .x = 20,
-//                 .y = 40
-//             }
-//         }
-//     };
+    const Person = struct {
+        name: []const u8,
+        age: u32,
+        is_active: bool,
+        location: ?Point,
+        pointsHashMap: ?Point,
+        tags: []const []const u8,
+        points: []const Point,
+    };
+
+    const human = Person{
+        .name = "Sayan",
+        .age = 100,
+        .is_active = true,
+        .location = .{
+            .x = 123.123,
+            .y = 123.123,
+        },
+        .points = &[_]Point{
+            .{
+                .x = 123.123,
+                .y = 123.123
+            }
+        },
+        .pointsHashMap = null,
+        .tags = &[_][]const u8{
+            "India", "US"
+        }
+    };
+
+    writer.writeAny(human) catch unreachable;
+    writer.deinit() catch unreachable;
+}
+
+fn prepareNodePool() void {
+    nodePool = mpack.Tree.Pool.init(alloc, 128) catch unreachable;
+}
+
+fn explicitWriteBenchmark(allocator: std.mem.Allocator) void {
+    for (0..100) |_| {
+        var buffer: [1024]u8 = undefined;
+        var map = std.StringArrayHashMap(f64).init(allocator);
+        defer map.deinit();
+
+        map.put("x", 123.123) catch unreachable;
+        map.put("y", 123.123) catch unreachable;
+
+        var writer = mpack.Writer.init(&buffer);
+
+        // Test explicit API
+        writer.startMap(7) catch unreachable;
+        
+        writer.writeString("name") catch unreachable;
+        writer.writeString("Sayan") catch unreachable;
+
+        writer.writeString("age") catch unreachable;
+        writer.writeUint32(100) catch unreachable;
+
+        writer.writeString("is_active") catch unreachable;
+        writer.writeBool(true) catch unreachable;
+
+        writer.writeString("location") catch unreachable;
+        writer.startMap(2) catch unreachable;
+        writer.writeString("x") catch unreachable;
+        writer.writeDouble(123.535) catch unreachable;
+        writer.writeString("y") catch unreachable;
+        writer.writeDouble(1234.1234) catch unreachable;
+        writer.finishMap() catch unreachable;
+
+        // Test writeHashmap
+        writer.writeString("pointsHashMap") catch unreachable;
+        writer.writeHashMap(f64, map) catch unreachable;
+
+        // Test writeAny
+        writer.writeAny("tags") catch unreachable;
+        writer.startArray(2) catch unreachable;
+        writer.writeAny("India") catch unreachable;
+        writer.writeAny("US") catch unreachable;
+        writer.finishArray() catch unreachable;
+
+        writer.writeAny("points") catch unreachable;
+        writer.startArray(1) catch unreachable;
+        writer.startMap(2) catch unreachable;
+        writer.writeAny("x") catch unreachable;
+        writer.writeAny(123.535) catch unreachable;
+        writer.writeAny("y") catch unreachable;
+        writer.writeAny(1234.1234) catch unreachable;
+        writer.finishMap() catch unreachable;
+        writer.finishArray() catch unreachable;
+
+        writer.finishMap() catch unreachable;
+
+        // Flush
+        writer.deinit() catch unreachable;
+    }
+}
+
+fn structSerializationBenchmark(_: std.mem.Allocator) void {
+    for (0..100) |_| {
+        var buffer: [1024]u8 = undefined;
+        var writer = mpack.Writer.init(&buffer);
+
+        const Point = struct {
+            x: f64,
+            y: f64,
+        };
+
+        const Person = struct {
+            name: []const u8,
+            age: u32,
+            is_active: bool,
+            location: ?Point,
+            pointsHashMap: ?Point,
+            tags: []const []const u8,
+            points: []const Point,
+        };
+
+        const human = Person{
+            .name = "Sayan",
+            .age = 100,
+            .is_active = true,
+            .location = .{
+                .x = 123.123,
+                .y = 123.123,
+            },
+            .points = &[_]Point{
+                .{
+                    .x = 123.123,
+                    .y = 123.123
+                }
+            },
+            .pointsHashMap = null,
+            .tags = &[_][]const u8{
+                "India", "US"
+            }
+        };
+
+        writer.writeAny(human) catch unreachable;
+        writer.deinit() catch unreachable;
+    }
+}
+
+fn treeParseBenchmark(allocator: std.mem.Allocator) void {
+    var tree = mpack.Tree.init(allocator, &readBuffer, null) catch unreachable;
+    defer (tree.deinit() catch unreachable);
+}
+
+fn treeParseWithPoolBenchmark(allocator: std.mem.Allocator) void {
+    var tree = mpack.Tree.init(allocator, &readBuffer, nodePool) catch unreachable;
+    defer (tree.deinit() catch unreachable);
+}
+
+fn treeReadBenchmark(allocator: std.mem.Allocator) void {
+    var tree = mpack.Tree.init(allocator, &readBuffer, null) catch unreachable;
+    defer (tree.deinit() catch unreachable);
+
+    const p1 = tree.getByPath("name") catch unreachable;
+    const p2 = tree.getByPath("age") catch unreachable;
+    const p3 = tree.getByPath("is_active") catch unreachable;
+    const p4  = tree.getByPath("location.x") catch unreachable;
+    const p5 = tree.getByPath("pointsHashMap") catch unreachable;
+    const p6 = tree.getByPath("tags[0]") catch unreachable;
+    const p7 = tree.getByPath("points[0].x") catch unreachable;
+
+    _ = p1;
+    _ = p2;
+    _ = p3;
+    _ = p4;
+    _ = p5;
+    _ = p6;
+    _ = p7;
+}
+
+fn treeCursorBenchmark(allocator: std.mem.Allocator) void {
+    var tree = mpack.Tree.init(allocator, &readBuffer, null) catch unreachable;
+    defer (tree.deinit() catch unreachable);
+    var cursor = tree.cursor() catch unreachable;
+
+    while (cursor.next() catch unreachable) |event| {
+        _ = event;
+    }
+}
+
+fn cursorBenchmark(_: std.mem.Allocator) void {
+    var reader = mpack.Reader.init(&readBuffer);
+    defer (reader.deinit() catch unreachable);
+    var cursor = reader.cursor();
     
-//     var timer1 = try std.time.Timer.start();
-//     try writer.serialize(person);
-//     const timeDone1 = timer1.read();
+    while (cursor.next() catch unreachable) |event| {
+        _ = event;
+    }
+}
 
-//     const stats = writer.stat();
+pub fn main() !void {
+    const stdout = std.io.getStdOut().writer();
+    var bench = zbench.Benchmark.init(alloc, .{});
+    defer bench.deinit();
 
-//     try writer.deinit();
+    // We re-use one Node pool across all runs
+    prepareNodePool();
 
-//     std.debug.print("done in {d} ns. written {d} of {d} bytes to buffer, {d} left\n", .{timeDone1, stats.buffer_used, stats.buffer_size, stats.buffer_remaining});
+    // Explicit write benchmark
+    try bench.add("explicit write x 100", explicitWriteBenchmark, .{});
 
-//     // Example usage
-//     const data = buffer[0..stats.buffer_used];
+    // Struct serialization benchmark
+    try bench.add("serialize struct x 100", structSerializationBenchmark, .{});
 
-//     var timer2 = try std.time.Timer.start();
-//     var tree = try Reader.init(allocator, data);
-//     defer (tree.deinit() catch |err| {
-//         std.debug.panic("Failed to de-initialize tree: {s}\n", .{@errorName(err)});
-//     });
+    // Tree parsing benchmark
+    try bench.add("tree: parse", treeParseBenchmark, .{.hooks = .{.before_all = prepareReadBuffer}});
 
-//     // Access nodes using path notation
-//     const v1 = try tree.root.getByPath("points[0].x");
-//     const timeDone2 = timer2.read();
-//     std.debug.print("v1 is {d}\n", .{try v1.getDouble()});
+    // Tree parsing benchmark with node pool
+    try bench.add("tree: parse w/ pool", treeParseWithPoolBenchmark, .{.hooks = .{.before_all = prepareReadBuffer}});
 
-//     const v2 = try tree.root.getByPath("tags[0]");
-//     std.debug.print("v2 is {s}\n", .{try v2.getString()});
+    // Tree reading benchmark
+    try bench.add("tree: read by path", treeReadBenchmark, .{.hooks = .{.before_all = prepareReadBuffer}});
 
-//     std.debug.print("read 1 entry in {d} ns\n", .{timeDone2});
+    // Tree cursor iterating benchmark
+    try bench.add("tree: cursor iterate", treeCursorBenchmark, .{.hooks = .{.before_all = prepareReadBuffer}});
 
-//     // Create a cursor
-//     var cursor = try Cursor.init(tree.root);
+    // Reader cursor iterating benchmark
+    try bench.add("reader: cursor iterate", cursorBenchmark, .{.hooks = .{.before_all = prepareReadBuffer}});
 
-//     // Traverse the tree
-//     while (try cursor.next()) |event| {
-//         switch (event.type) {
-//             .MapStart => {
-//                 std.debug.print("Map start (size: {})\n", .{event.size.?});
-//             },
-//             .MapKey => {
-//                 // The next event will contain the actual key value
-//                 // std.debug.print("Map key start\n", .{});
-//             },
-//             .MapValue => {
-//                 // The next event will contain the actual value
-//                 // std.debug.print("Map value start\n", .{});
-//             },
-//             .MapEnd => {
-//                 std.debug.print("Map end\n", .{});
-//             },
-//             .ArrayStart => {
-//                 std.debug.print("Array start (size: {})\n", .{event.size.?});
-//             },
-//             .ArrayItem => {
-//                 // The next event will contain the actual item
-//             },
-//             .ArrayEnd => {
-//                 std.debug.print("Array end\n", .{});
-//             },
-//             .String => {
-//                 std.debug.print("String: {s}\n", .{event.string_val.?});
-//             },
-//             .Int => {
-//                 std.debug.print("Integer: {}\n", .{event.int_val.?});
-//             },
-//             .Boolean => {
-//                 std.debug.print("Boolean: {}\n", .{event.bool_val.?});
-//             },
-//             else => {
-//                 std.debug.print("Other type {any}\n", .{event.type});
-//             }
-//         }
-//     }
-
-// }
+    try stdout.writeAll("\n");
+    try bench.run(stdout);
+}
--- build.zig
+++ build.zig
@@ -51,6 +51,8 @@ pub fn build(b: *std.Build) void {
     });
 
     // Benchmarks
+    const zBench = b.dependency("zbench", .{});
+
     const bench = b.addExecutable(.{
         .name = "mpack-bench",
         .root_source_file = b.path("benchmark/main.zig"),
@@ -60,6 +62,7 @@ pub fn build(b: *std.Build) void {
 
     bench.linkLibC();
     bench.root_module.addImport("mpack", mpack);
+    bench.root_module.addImport("zbench", zBench.module("zbench"));
 
     // b.installArtifact(bench);
 
