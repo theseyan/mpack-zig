45da26e9758847bacb6c17cddee035bb5062440d
theseyan <sayanjyotidasworkmail@gmai.com>
2025-01-15 13:06:47 +0530

    add build options to disable read/write tracking for better performance

--- build.zig
+++ build.zig
@@ -18,6 +18,18 @@ pub fn build(b: *std.Build) void {
     buildOpts.addOption(bool, "builder_api", false);
     buildOpts.addOption(bool, "expect_api", false);
 
+    // Enables compound type size tracking for readers.
+    // This ensures that the correct number of elements or bytes are read from a compound type.
+    // Keep enabled unless maximum performance is required instead of safety.
+    const read_tracking = b.option(bool, "read_tracking", "Use Read Tracking for safety (default: true)") orelse true;
+    buildOpts.addOption(bool, "read_tracking", read_tracking);
+
+    // Enables compound type size tracking for writers.
+    // This ensures that the correct number of elements or bytes are written in a compound type.
+    // Note that without write tracking enabled, it is possible for buggy code to emit invalid MessagePack without flagging an error by writing the wrong number of elements or bytes in a compound type. With tracking enabled, MPack will catch such errors and break on the offending line of code.
+    const write_tracking = b.option(bool, "write_tracking", "Use Write Tracking for safety") orelse true;
+    buildOpts.addOption(bool, "write_tracking", write_tracking);
+
     // Whether to use mimalloc allocator
     // If this is true, then mimalloc must be statically linked.
     const use_mimalloc = b.option(bool, "use_mimalloc", "Use mimalloc allocator (requires static linking)") orelse false;
@@ -47,6 +59,9 @@ pub fn build(b: *std.Build) void {
 
             // We want to optimize for speed.
             "-DMPACK_OPTIMIZE_FOR_SIZE=0",
+
+            if (!read_tracking) "-DMPACK_READ_TRACKING=0" else "",
+            if (!write_tracking) "-DMPACK_WRITE_TRACKING=0" else "",
 
             // Use mimalloc allocator if enabled
             if (use_mimalloc) "-includemimalloc-override.h" else "",
